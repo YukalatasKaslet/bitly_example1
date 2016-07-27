ec92bc4e5fb61cfe183f1db4e438f8f8fd4d3e96
codeamac01 <codeamac01@gmail.com>
2016-07-27 17:22:20 -0500

    validaciones corregidas, se crea el short url(string) aun no se asigna al atributo correspondiente

--- app/models/shortened_url.rb
+++ app/models/shortened_url.rb
@@ -4,10 +4,10 @@ class ShortenedUrl < ActiveRecord::Base
   before_create :create_short_url
   # Remember to create a migration!
 
-  #Intento de validaciones xD
+  #validaciones correctas :D
   validates :long_url, presence: true, uniqueness: true, format: { with: URI.regexp }
 
-
+#Intento de validaciones xD
   #def valid?(url)
   #  uri = URI.parse(url)
   #  uri.kind_of?(URI::HTTP)
@@ -21,5 +21,7 @@ class ShortenedUrl < ActiveRecord::Base
   private
   def create_short_url
     #aquí debería crear la url corta pero aún no sabo como xD jajajaja
+    short_url = "Yuka" + (1..6).map{ ('a'..'z').to_a[rand(26)]}.join
+
   end
 end
