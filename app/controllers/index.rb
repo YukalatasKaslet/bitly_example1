003a8df2b7ec82506dbcd2bc84e09f7c5a6087c3
codeamac01 <codeamac01@gmail.com>
2016-07-27 18:30:03 -0500

    controlador terminado, ya redirige a la página correcta

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -12,17 +12,30 @@ post '/urls' do
   #find_or_create_by(attributes, &block) Find de FIRST record with the given attributes or creates a record with the attributes if one is not found.
   #This method it runs first a SELECT, and if there are no results an INSERT is attempted.
   @url = ShortenedUrl.find_or_create_by(long_url: params[:url_original])
-  p params
+  #p params #contiene un hash con url_original
+
+  #tambien se puede hacer así
+  #@url = ShortenedUrl.create(long_url: params[:url_original])
+  #sin embargo si el objeto ya existe hay que hacer un if extra ya que se regresa un "objeto" con id = nil oseasele no se guardó por qué ya existe.
+
   if @url.valid?
     @urls = ShortenedUrl.all
     erb :index
   else
-    erb :error
+    @error = true
+    erb :index
   end
   
 end
 
 get '/:short_url' do
   #redirige a la Url original
+  short_url = params[:short_url]
+  url = ShortenedUrl.find_by(short_url: short_url)
+  if url
+    redirect to( url.long_url )
+  else
+    erb :error
+  end
 
 end
