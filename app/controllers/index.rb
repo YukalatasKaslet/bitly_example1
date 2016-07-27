1ad8b28a9cd94eb17ec67c33af360b0edcabbc33
codeamac01 <codeamac01@gmail.com>
2016-07-27 17:23:10 -0500

    se añadio lineas para que muestre los urls ingresados

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -2,6 +2,7 @@ get '/' do
   #aquí está el submit del long_url
   # La siguiente linea hace render de la vista
   # que esta en app/views/index.erb
+  @urls = ShortenedUrl.all
   erb :index
 end
 
@@ -11,7 +12,9 @@ post '/urls' do
   #find_or_create_by(attributes, &block) Find de FIRST record with the given attributes or creates a record with the attributes if one is not found.
   #This method it runs first a SELECT, and if there are no results an INSERT is attempted.
   @url = ShortenedUrl.find_or_create_by(long_url: params[:url_original])
+  p params
   if @url.valid?
+    @urls = ShortenedUrl.all
     erb :index
   else
     erb :error
